b06aa433d6a085525b44073dce02e62a7859a103
oluobamzy <oluobamzy@gmail.com>
2023-08-21 18:28:20 -0400

    exercise 7 added

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,62 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 7: Validations for both models
+
+# 1. Add validations to two models to enforce the following business rules:
+
+# - Employees must always have a first name present
+# - Employees must always have a last name present
+# - Employees have a hourly_rate that is a number (integer) between 40 and 200
+# - Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
+# - Stores must always have a name that is a minimum of 3 characters
+# - Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# - BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a [custom validation method](http://guides.rubyonrails.org/active_record_validations.html#custom-methods) - **don't** use a `Validator` class)
+
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+#validate store first name
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+
+
+class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_have_apparel
+
+  private
+
+  def must_have_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:base, "Store must carry at least one type of apparel")
+    end
+  end
+end
+
+
+puts "Enter the name of the store:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+new_store.save
+
+if new_store.errors.any?
+  puts "Store could not be saved due to the following errors:"
+  new_store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+else
+  puts "Store '#{store_name}' successfully created!"
+end
