eecc29f010ebfa30270f82e6e20612f0375f4c2a
oluobamzy <oluobamzy@gmail.com>
2023-08-21 16:51:50 -0400

    remove comments add exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,9 +1,4 @@
 require_relative '../setup'
-# 1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
-
-# - Burnaby (annual_revenue of 300000, carries men's and women's apparel)
-# - Richmond (annual_revenue of 1260000 carries women's apparel only)
-# - Gastown (annual_revenue of 190000 carries men's apparel only)
 puts "Exercise 1"
 puts "----------"
 
